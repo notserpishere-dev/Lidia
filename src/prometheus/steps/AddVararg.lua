6fe772b9344148b67e46f5d3bdc68f6cf5051d5c
notserpishere-dev <notserpishere@gmail.com>
2026-05-10 17:48:50 +0200

    Update AddVararg.lua

--- src/prometheus/steps/AddVararg.lua
+++ src/prometheus/steps/AddVararg.lua
@@ -1,30 +1,41 @@
--- This Script is Part of the Prometheus Obfuscator by levno-710
---
--- AddVararg.lua
---
--- This Script provides a Simple Obfuscation Step that wraps the entire Script into a function
+local Step = require("prometheus.step")
+local Ast = require("prometheus.ast")
+local visitast = require("prometheus.visitast")
+local AstKind = Ast.AstKind
 
-local Step = require("prometheus.step");
-local Ast = require("prometheus.ast");
-local visitast = require("prometheus.visitast");
-local AstKind = Ast.AstKind;
+local AddVararg = Step:extend()
+AddVararg.Description = "Appends a vararg (...) to every function signature to complicate static analysis."
+AddVararg.Name = "Add Vararg"
 
-local AddVararg = Step:extend();
-AddVararg.Description = "This Step Adds Vararg to all Functions";
-AddVararg.Name = "Add Vararg";
+AddVararg.SettingsDescriptor = {
+    Enabled = {
+        type = "boolean",
+        default = true,
+        description = "Enable adding varargs to all functions."
+    }
+}
 
-AddVararg.SettingsDescriptor = {}
-
-function AddVararg:init(_) end
+function AddVararg:init(settings)
+    self.settings = settings or { Enabled = true }
+end
 
 function AddVararg:apply(ast)
-	visitast(ast, nil, function(node)
-        if node.kind == AstKind.FunctionDeclaration or node.kind == AstKind.LocalFunctionDeclaration or node.kind == AstKind.FunctionLiteralExpression then
-            if #node.args < 1 or node.args[#node.args].kind ~= AstKind.VarargExpression then
-                node.args[#node.args + 1] = Ast.VarargExpression();
+    if not self.settings.Enabled then return end
+
+    visitast(ast, nil, function(node)
+        local isFunction = node.kind == AstKind.FunctionDeclaration 
+                        or node.kind == AstKind.LocalFunctionDeclaration 
+                        or node.kind == AstKind.FunctionLiteralExpression
+
+        if isFunction then
+            node.args = node.args or {}
+            local hasVararg = #node.args > 0 and node.args[#node.args].kind == AstKind.VarargExpression
+
+            if not hasVararg then
+                table.insert(node.args, Ast.VarargExpression())
             end
         end
     end)
 end
 
-return AddVararg;
+return AddVararg
